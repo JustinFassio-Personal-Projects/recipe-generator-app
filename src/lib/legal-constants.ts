24ce681716933e46fc0bc1c229b6479b0bf53126
AI Workout Gen <justin@aiworkoutgenerator.com>
2025-11-11 16:51:27 -0800

    feat: add sanctuary health legal compliance flow

---
+++ src/lib/legal-constants.ts
@@ -0,0 +1,6 @@
+// Legal document version constants
+// When updating terms or privacy policy, increment these versions
+// All users will be prompted to re-accept on next login
+
+export const CURRENT_TERMS_VERSION = '1.0';
+export const CURRENT_PRIVACY_VERSION = '1.0';
